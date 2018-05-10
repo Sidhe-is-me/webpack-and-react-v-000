815adc91a5e9aefb4d7ffb9d65968aba5c5bfa7b
Yvonne Dreptate <YvonneDreptate@gmail.com>
2018-05-10 11:58:34 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -12,6 +12,9 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+
+          IT would be great if I knew what I was supposed to be updating for this lab..
+    
         </p>
       </div>
     );
